adda45e2212740c3a3b288c26e3d11d216d5f251
Travis Matthews <trav.matth@gmail.com>
2016-11-22 15:06:51 -0500

    tidying up

---
+++ tools/webpack.config.deploy.babel.js
@@ -0,0 +1,18 @@
+import merge from 'webpack-merge'
+import S3Plugin from 'webpack-s3-plugin'
+import productionBuild from 'webpack.config.prod.babel'
+
+export default validate(merge(productionBuild, {
+  plugins: [
+    new S3Plugin({
+      s3Options: {
+        // accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        // secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        // region: 'HARDCODED'
+      },
+      s3UploadOptions: {
+        // Bucket: 'HARDCODED'
+      }
+    })
+  ]
+})
