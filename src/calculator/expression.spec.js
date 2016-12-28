6c4ca1b69fe9b0264e4761abe1b79a206a65e398
Travis Matthews <trav.matth@gmail.com>
2016-12-27 21:53:25 -0500

    added ava tests to calculator, refactored some localweather

--- src/calculator/expression.spec.js
+++ src/calculator/expression.spec.js
@@ -1,137 +1,126 @@
 /* @flow */
+import test from 'ava'
+
 import LogicUnit from './LogicUnit'
 
-('LogicUnit', () => {
-  let expr, infix
-    
-  (() => {
-      expr = new LogicUnit()
-      infix = []
-  });
+let expr, infix
 
-  ('update', () => {
-    ('decimals can be entered', () => {
-      expr.update('9')
-      expr.update('.')
-      expr.update('3')
-      expr.update('3')
-      (expr.expression[0]).to.equal('9.33')
-    });
-
-    ('decimals can be deleted', () => {
-      expr.update('9')
-      expr.update('.')
-      expr.update('3')
-      expr.update('3')
-      expr.clear()
-      (expr.getExpression()).to.equal('9.3')
-    });
-
-    ('decimal can be deleted', () => {
-      expr.update('9')
-      expr.update('.')
-      expr.clear()
-      (expr.getExpression()).to.equal('9')
-    });
-  });
-
-  ('Compute', () => {
-    ('1 + 1 should work', () => {
-      expr.update('1')
-      expr.update('+')
-      expr.update('1')
-      expr.compute()
-      (expr.getExpression()).to.equal('2')
-    });
-
-    ('2 * 3 should work', () => {
-      expr.update('2')
-      expr.update('*')
-      expr.update('3')
-      expr.compute()
-      (expr.getExpression()).to.equal('6')
-    });
-
-    ('4 / 2 should work', () => {
-      expr.update('4')
-      expr.update('/')
-      expr.update('2')
-      expr.compute()
-      (expr.getExpression()).to.equal('2')
-    });
-
-    ('4 - 2 should work', () => {
-      expr.update('4')
-      expr.update('-')
-      expr.update('2')
-      expr.compute()
-      (expr.getExpression()).to.equal('2')
-    });
-
-    ('6 / 3 * (4 - 6) + 5 should work', () => {
-      expr.update('6')
-      expr.update('/')
-      expr.update('3')
-      expr.update('*')
-      expr.update('(')
-      expr.update('4')
-      expr.update('-')
-      expr.update('6')
-      expr.update(')')
-      expr.update('+')
-      expr.update('5')
-      expr.compute()
-      (expr.getExpression()).to.equal('1')
-    });
-
-    ('symbols can be entered', () => {
-      const expected = ['2.718', '3.14', '0.693']
-      expr.update('E')
-      expr.update('PI')
-      expr.update('LN2')
-      (expr.expression).to.eql(expected)
-    })
-  });
-
-  ('functions should work', () => {
-    ('SIN should compute correctly', () => {
-      expr.update('SIN')
-      expr.update('0')
-      expr.update(')')
-      expr.compute()
-      (expr.getExpression()).to.equal('0')
-    })
-
-    ('COS should compute correctly', () => {
-      expr.update('COS')
-      expr.update('0')
-      expr.update(')')
-      expr.compute()
-      (expr.getExpression()).to.equal('1')
-    })
-
-    ('TAN should compute correctly', () => {
-      expr.update('TAN')
-      expr.update('1')
-      expr.update(')')
-      expr.compute()
-      (expr.getExpression()).to.equal('1.55741')
-    })
-
-    ('LOG should compute correctly', () => {
-      expr.update('LOG')
-      expr.update('1')
-      expr.update(')')
-      expr.compute()
-      (expr.getExpression()).to.equal('0')
-    })
-  });
+test.beforeEach('LogicUnit', () => {
+  expr = new LogicUnit()
+  infix = []
 });
 
-.skip('Array', function() {
-  ('#indexOf()', function() {
-    ('should return -1 when the value is not present', function() {
-      // .equal(-1, [1,2,3].indexOf(4));
-    });
-  });
+//UPDATING
+test('decimals can be entered', t => {
+  expr.update('9')
+  expr.update('.')
+  expr.update('3')
+  expr.update('3')
+  t.is(expr.expression[0], '9.33')
 });
+
+test('decimals can be deleted', t => {
+  expr.update('9')
+  expr.update('.')
+  expr.update('3')
+  expr.update('3')
+  expr.clear()
+  t.is(expr.getExpression(), '9.3')
+});
+
+test('decimal can be deleted', t => {
+  expr.update('9')
+  expr.update('.')
+  expr.clear()
+  t.is(expr.getExpression(), '9')
+});
+
+//ARITHMETIC
+test('1 + 1 should work', t => {
+  expr.update('1')
+  expr.update('+')
+  expr.update('1')
+  expr.compute()
+  t.is(expr.getExpression(), '2')
+});
+
+test('2 * 3 should work', t => {
+  expr.update('2')
+  expr.update('*')
+  expr.update('3')
+  expr.compute()
+  t.is(expr.getExpression(), '6')
+});
+
+test('4 / 2 should work', t => {
+  expr.update('4')
+  expr.update('/')
+  expr.update('2')
+  expr.compute()
+  t.is(expr.getExpression(), '2')
+});
+
+test('4 - 2 should work', t => {
+  expr.update('4')
+  expr.update('-')
+  expr.update('2')
+  expr.compute()
+  t.is(expr.getExpression(), '2')
+});
+
+test('6 / 3 * (4 - 6) + 5 should work', t => {
+  expr.update('6')
+  expr.update('/')
+  expr.update('3')
+  expr.update('*')
+  expr.update('(')
+  expr.update('4')
+  expr.update('-')
+  expr.update('6')
+  expr.update(')')
+  expr.update('+')
+  expr.update('5')
+  expr.compute()
+  t.is(expr.getExpression(), '1')
+});
+
+//FUNCTIONS
+test('symbols can be entered', t => {
+  const expected = ['2.718', '3.14', '0.693']
+  expr.update('E')
+  expr.update('PI')
+  expr.update('LN2')
+  (expr.expression).to.eql(expected)
+})
+
+test('SIN should compute correctly', t => {
+  expr.update('SIN')
+  expr.update('0')
+  expr.update(')')
+  expr.compute()
+  t.is(expr.getExpression(), '0')
+})
+
+test('COS should compute correctly', t => {
+  expr.update('COS')
+  expr.update('0')
+  expr.update(')')
+  expr.compute()
+  t.is(expr.getExpression(), '1')
+})
+
+test('TAN should compute correctly', t => {
+  expr.update('TAN')
+  expr.update('1')
+  expr.update(')')
+  expr.compute()
+  t.is(expr.getExpression(), '1.55741')
+})
+
+test('LOG should compute correctly', t => {
+  expr.update('LOG')
+  expr.update('1')
+  expr.update(')')
+  expr.compute()
+  t.is(expr.getExpression(), '0')
+})
--- src/localweather/index.spec.js
+++ src/localweather/index.spec.js
@@ -3,14 +3,14 @@ import test from 'ava'
 import jsdom from 'jsdom-global/register'
 import fetchMock from 'fetch-mock';
 import { response, data } from './mockdata.spec'
-import { serializeDocument } from 'jsdom'
+// import { serializeDocument } from 'jsdom'
 import ResponseError from '../common/utils'
 
 //Model under test
 import { fetchWeather, openweatherApiParams , endpoint, main } from './index'
 import { serialize } from '../common/utils'
 
-
+/***********************************SETUP**************************************/
 const url = 'http://api.openweathermap.org/' +
   'data/2.5/forecast?' +
   'lat=0&' + 
@@ -18,39 +18,35 @@ const url = 'http://api.openweathermap.org/' +
   'units=imperial&' +
   'APPID=c26ef1df98c449f37f8f199738ce74c7'
 
-test.before('before', () => {
-  console.log('before called')
-  if (typeof document !== 'undefined') return
+jsdom.jsdom( 
+  '<!DOCTYPE html>' + 
+  '<html>' +
+    '<body>' +
+      '<li class="cell">' +
+      '</li>' +
+    '</body>' + 
+  '</html>'
+)
 
-  document = jsdom.jsdom( 
-    '<!DOCTYPE html>' + 
-    '<html>' +
-      '<body>' +
-        '<li class="cell">' +
-        '</li>' +
-      '</body>' + 
-    '</html>'
-  )
-
-  window = document.defaultView
-  navigator = {
-    geolocation: {
-      getCurrentPosition: (success, error, options) => {
-        success({ coords: { latitude: 0,longitude: 0 } });
-      }
+window = document.defaultView
+window.navigator = {
+  geolocation: {
+    getCurrentPosition: (success, error, options) => {
+      success({ coords: { latitude: 0,longitude: 0 } });
     }
-  }; 
+  }
+}; 
 
-  Object.keys(window).forEach((key) => {
-    if (!(key in global)) {
-      global[key] = window[key];
-    }
-  });
-})
+Object.keys(window).forEach((key) => {
+  if (!(key in global)) {
+    global[key] = window[key];
+  }
+});
 
 test.after.always('after', () => {
   fetchMock.restore(); 
 })
+/*******************************END SETUP**************************************/
 
 test('fetchWeather can return parsed json', async t => {
   fetchMock.post(url, response);
@@ -61,25 +57,24 @@ test('fetchWeather can return parsed json', async t => {
   const { now, ...object } = data
 
   t.deepEqual(rest, object)
-  t.equal(typeof date, 'number')
+  t.is(date, 'number')
 });
 
 test('fetchWeather throws appropriately', async t => {
   fetchMock.post(url, { status: 404, body: data });
 
   const url = serialize(endpoint, params(0, 0))
-  const call = async () => await fetchWeather(url)
+  const error = await t.throws(fetchWeather(url))
 
-  t.throws(await call, ResponseError)
+  t.is(error.response.url, url)
 });
 
 test('main() obtains user coordinates and populates list elements', async t => {
   fetchMock.post(url, data);
 
   const time = 0
-
-  //call function
   const t = await main(time)
+
   // not sure what i'll be measuring yet
   t.pass()
 });
