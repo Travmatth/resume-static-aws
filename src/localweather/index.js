6e06bdd572cd79ebeb5f00b00ef34fe09a588d10
Travis Matthews <trav.matth@gmail.com>
2016-12-31 18:06:12 -0500

    started to flesh out the display; refactored the page & js, need to test and flowcheck

--- src/localweather/index.js
+++ src/localweather/index.js
@@ -1,56 +1,79 @@
 /* @flow */
 
-//Types
-import { 
-  DailyForecast, Weather, FiveDayForecast, Forecast, Daily, Coordinates
-} from './index.js.flow' 
+/*
+  Types
+*/
 
-import { serialize, ResponseError } from '../common/utils'
-import { parseTime } from './constants'
+import { 
+  DailyForecast, 
+  Weather, 
+  FiveDayForecast, 
+  Forecast, 
+  Daily, 
+  Coordinates, 
+  ApiParams,
+} from './index.js.flow';
+
+/*
+  Libraries
+*/
+
+import { serialize, ResponseError } from '../common/utils';
+import { parseTime } from './constants';
+
+/*
+  Constants
+*/
 
 //Initiliazed in DOMContentLoaded
-let cells: ?NodeList<HTMLElement>
+let cells: ?NodeList<HTMLElement>;
 
-export const openweatherApiParams = (lat: number, lon: number): Object => ({
+export const endpoint = 'http://api.openweathermap.org/data/2.5/forecast';
+
+export const openweatherApiParams = (lat: number, lon: number): ApiParams => ({
   'lat': lat,
   'lon': lon,
   'units': 'imperial',
   'APPID': 'c26ef1df98c449f37f8f199738ce74c7',
-})
+});
 
-export const endpoint = 'http://api.openweathermap.org/data/2.5/forecast' 
+/*
+  Start program
+*/
+
+export const contentLoadedListener = async (
+  cells: NodeList<HTMLElement>, 
+  time: number = 500
+): void =>  {
+  cells.forEach(el => console.log(el));
+  const launch = () => navigator.geolocation.getCurrentPosition(getWeather);
+  setTimeout(launch, time);
+}
+
+if (document !== undefined) {
+  const cells = document.querySelectorAll('.cell');
+  const listener = contentLoadedListener.bind(undefined, cells);
+  document.addEventListener('DOMContentLoaded', listener);
+}
 
 export const getWeather = async (location: Coordinates): void  => {
-  const { latitude, longitude, } = location.coords 
+  const { latitude, longitude, } = location.coords; 
   
-  const resource: string = serialize(
-    endpoint, 
-    openweatherApiParams(latitude, longitude)
-  )
+  const opts = openweatherApiParams(latitude, longitude);
+  const resource: string = serialize(endpoint, opts);
 
   // Call API, parse response
   try {
     const weather: any = await fetchWeather(resource)
-
-  // Call API, parse response, generate data, update DOM
-  // updateDOM(weather, cells)
+    updateDOM(weather, cells)
   } catch(error) {
     console.error('error', error)
   }
-}
+};
 
-export const main = async (cells: NodeList<HTMLElement>, time: number = 500): void =>  {
-  cells.forEach(el => console.log(el))
-
-  setTimeout(() => navigator.geolocation.getCurrentPosition(getWeather), time);
-}
-
-if (document !== undefined) {
-  const cells = document.querySelectorAll('.cell')
-  const listener = main.bind(undefined, cells)
-  document.addEventListener('DOMContentLoaded', listener);
-}
-
+/*
+  Network call
+*/
 
 export const fetchWeather = async (url: string): Weather => {
   // Fetch initial resource after delay
@@ -62,17 +85,71 @@ export const fetchWeather = async (url: string): Weather => {
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
-    },
+    }
   }
   
   return await fetch(url, opts)
     .then(checkResponse)
     .then(processWeather)
-    .catch(err => { 
-      // console.log('fetch err', err);
-      throw err 
-    })
-}
+    .catch(err => throw err)
+};
+
+/*
+  DOM Interaction
+*/
+
+
+export const updateDOM = (results: Array<Object>): void => {
+  results.map(result => {
+    if (cells) cells.forEach(cell => {
+      //Iter over the table data's containing the content
+      cell.children.forEach(element => {
+
+        //create cells using info from result
+        // { weatherJson.forecasts.map((val, i) => {
+        //   const { icon, temp, day, time, weather, description, } = val
+
+        switch (element.className) {
+          case 'day': 
+            element.textContent = result.day
+            break
+
+          case 'time': 
+            element.textContent = result.time
+            break
+
+          case 'measurement': 
+            //   const measurement = active('CELSIUS') 
+            //     ? `${temp.celsius}°C`
+            //     : `${temp.farenheit}°F`
+            //     img key={icon} src={`${icon}`}
+            
+            // element.textContent = result.measurement
+            element.textContent = ''
+            break
+
+          case 'icon': 
+            //Icon td contains both an img element & td.weather
+            break
+
+          default: 
+            console.error('Unrecognized HTMLElement', element)
+            break
+        }
+      })
+
+      //Make visible
+
+      // extract classname, make visible if applicable
+      // const { className } = cell
+      // toggleVisibility(className)
+    });
+  });
+};
+
+/* 
+  Supporting Functions
+*/
 
 export const checkResponse = async (response: Response): FiveDayForecast => {
   if (response.status < 200 || response.status >= 400) { 
@@ -80,7 +157,7 @@ export const checkResponse = async (response: Response): FiveDayForecast => {
   }
 
   return await (response.json(): FiveDayForecast)
-}
+};
 
 export const processWeather = (data: FiveDayForecast): Weather => ({
   city: data.city.name,
@@ -89,11 +166,11 @@ export const processWeather = (data: FiveDayForecast): Weather => ({
     .map(processForecasts)
     .map(parseTime)
     .map(stripDateIfRedundant)
-})
+});
 
 export const convertFahrenheitToCelsius = (temp: number) => (
   Math.round(temp * 1.8 + 32)
-)
+);
 
 export const processForecasts = (outlook: Forecast): DailyForecast => ({
   icon: `http://openweathermap.org/img/w/${outlook.weather[0].icon}.png`,
@@ -115,27 +192,17 @@ export const stripDateIfRedundant = (
   seq: Array<Daily>
 ): Daily => {
   const { day, ...rest } = today
-  if (index > 0 && seq[index - 1].day === day) {
+  const dateRedundant = index > 0 && seq[index - 1].day === day 
+
+  if (dateRedundant) {
     return { day: '', ...rest }
   } else {
     return { day, ...rest }
   }
 } 
 
-export const updateDOM = (results: Array<Object>): void => {
-  results.map(result => {
-    if (cells) cells.forEach(cell => {
-      //create cells using info from result
-
-      // extract classname, make visible if applicable
-      // const { className } = cell
-      // toggleVisibility(className)
-    })
-  })
-}
-
-export const toggleVisibility = (css: string): void => {
-  css.match(/hide/)
+export const toggleVisibility = (css: string, toggled: boolean): void => {
+  toggled
     ? css.replace(/hide/, 'show')
     : css.replace(/show/, 'hide')
 }
