919893fde6c7ad4f34be818b6a403a95f245621c
Travis Matthews <trav.matth@gmail.com>
2017-01-11 20:14:10 -0500

    cleaned unneccessary comment

--- src/common/utils.js
+++ src/common/utils.js
@@ -18,7 +18,6 @@ export function serialize(url: string, params: Object) {
   return url + query.join("&");
 }
 
-
 export class ResponseError extends ExtendableError {
     response: Response;
     
@@ -27,45 +26,3 @@ export class ResponseError extends ExtendableError {
       this.response = response
     }
 }
-
-//       <div key='partial' className='weather'>
-//         <div className='control-bar'>
-//           <button onClick={() => toggle('FARENHEIT')} className={farenheit}>
-//             Farenheit
-//           </button>
-//           <button onClick={() => toggle('CELSIUS')} className={celsius}>
-//             Celsius
-//           </button>
-//           <h1 className='heading'>{ weather.city }</h1>
-//         </div>
-//         <table>
-//           <thead>
-//             <tr>
-//               <td>Day</td>
-//               <td>Time</td>
-//               <td>Temp</td>
-//               <td/>
-//               <td>Forecast</td>
-//             </tr>
-//           </thead>
-//           <tbody>
-//             { weatherJson.forecasts.map((val, i) => {
-//               const { icon, temp, day, time, weather, description, } = val
-//               const measurement = active('CELSIUS') 
-//                 ? `${temp.celsius}°C`
-//                 : `${temp.farenheit}°F`
-
-
-//               return (
-//                 <tr key={i}>
-//                   <td>{ day }</td>
-//                   <td>{ time }</td>
-//                   <td>{ measurement }</td>
-//                   <td><img key={icon} src={`${icon}`}/></td>
-//                   <td>{ weather }: { description }</td>
-//                 </tr>
-//               )})
-//             }
-//           </tbody>
-//         </table>
-//       </div>
