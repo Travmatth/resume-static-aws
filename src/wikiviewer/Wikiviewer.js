f9cfee73f3b50de66eee100f76581e316b8cba0a
Travis Matthews <trav.matth@gmail.com>
2017-03-24 15:25:26 -0400

    moved some types into defs

--- src/wikiviewer/Wikiviewer.js
+++ src/wikiviewer/Wikiviewer.js
@@ -1,22 +1,25 @@
 import { endpoint, params } from './wikiviewer.constants';
-import type { WikiSearchResult, WikiPage, WikiInit } from './wikiviewer.types';
+import type {
+  WikiSearchResult,
+  WikiPage,
+  WikiInit,
+  Headings,
+  Paragraphs,
+  Searches,
+} from './wikiviewer.types';
 import { serialize, ResponseError } from '../common/utils';
 
 export default class WikiViewer {
   query: Array<string>;
-  searchInput: HTMLInputElement;
-  randomButton: HTMLButtonElement;
-  searchButton: HTMLButtonElement;
-  nodes: HTMLCollection<HTMLElement>;
 
   constructor() {
     this.query = [];
   }
 
   updateDOM(
-    searchResults: ?Array<WikiPage>,
-    headings: HTMLCollection<HTMLElement>,
-    paragraphs: HTMLCollection<HTMLParagraphElement>,
+    searchResults: Searches,
+    headings: Headings,
+    paragraphs: Paragraphs,
   ) {
     if (searchResults) {
       let node, result, imgNode;
@@ -38,10 +41,7 @@ export default class WikiViewer {
     }
   }
 
-  search(
-    headings: HTMLCollection<HTMLElement>,
-    paragraphs: HTMLCollection<HTMLParagraphElement>,
-  ) {
+  search(headings: Headings, paragraphs: Paragraphs) {
     const { query } = this;
     if (!query.length === 0) return;
 
@@ -49,7 +49,10 @@ export default class WikiViewer {
     return fetch(serialize(endpoint, params))
       .then(checkHeaders)
       .then(processWikis)
-      .catch(err => null);
+      .catch(err => {
+        console.error('Fetch failed', err);
+        return null;
+      });
   }
 
   randomHandler(win: Window) {
@@ -58,26 +61,17 @@ export default class WikiViewer {
     };
   }
 
-  searchHandler(
-    headings: HTMLCollection<HTMLElement>,
-    paragraphs: HTMLCollection<HTMLParagraphElement>,
-  ) {
+  searchHandler(headings: Headings, paragraphs: Paragraphs) {
     return () => {
       this.refreshResults(headings, paragraphs);
     };
   }
 
-  async refreshResults(
-    headings: HTMLCollection<HTMLElement>,
-    paragraphs: HTMLCollection<HTMLParagraphElement>,
-  ) {
-    this.updateDOM(await this.search(), headings, paragraphs);
+  async refreshResults(headings: Headings, paragraphs: Paragraphs) {
+    this.updateDOM(await this.search(), Headings, paragraphs);
   }
 
-  keypressHandler(
-    headings: HTMLCollection<HTMLElement>,
-    paragraphs: HTMLCollection<HTMLParagraphElement>,
-  ) {
+  keypressHandler(headings: Headings, paragraphs: Paragraphs) {
     return async (event: Event) => {
       if (event.key === 'Enter') {
         this.refreshResults(headings, paragraphs);
--- src/wikiviewer/wikiviewer.types.js
+++ src/wikiviewer/wikiviewer.types.js
@@ -1,31 +1,35 @@
-export type WikiContinue {
-  "gsroffset":10,
-  "continue":"gsroffset||"
+export type Paragraphs = HTMLCollection<HTMLParagraphElement>;
+export type Headings = HTMLCollection<HTMLElement>;
+export type Searches = ?Array<WikiPage>;
+
+export type WikiContinue = {
+  'gsroffset': 10,
+  'continue': 'gsroffset||',
 };
 
-export type WikiPage {
-  "pageid": number;
-  "ns": number;
-  "title": string;
-  "index": number;
-  "extract": string; 
-}
+export type WikiPage = {
+  'pageid': number,
+  'ns': number,
+  'title': string,
+  'index': number,
+  'extract': string,
+};
 
-export type Limits {
-  "extracts":20
-}
+export type Limits = {
+  'extracts': 20,
+};
 
-export type WikiPages {|
+export type WikiPages = {|
   [string]: WikiPage,
-  "limits": Limits;
+  'limits': Limits,
 |};
 
-export type QueryResult {
-  "pages": WikiPages;  
+export type QueryResult = {
+  'pages': WikiPages,
 };
 
-export type WikiSearchResult {|
-  "batchcomplete": ""|true,
-  "continue": WikiContinue;
-  "query": QueryResult;
+export type WikiSearchResult = {|
+  'batchcomplete': '' | true,
+  'continue': WikiContinue,
+  'query': QueryResult,
 |};
