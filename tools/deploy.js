6afd2293a6f7880e4195056a1acaedb1c4d3ec63
Travis Matthews <trav.matth@gmail.com>
2017-04-25 10:44:42 -0400

    added custom deploy script

--- tools/deploy.js
+++ tools/deploy.js
@@ -1,4 +1,44 @@
 #! /usr/bin/env node
-
+const path = require('path');
+const s3 = require('s3');
 const s3Opts = require('../aws.js');
-console.log(s3Opts);
+
+var client = s3.createClient({
+  s3Options: {
+    accessKeyId: s3Opts.ACCESS_KEY_ID,
+    secretAccessKey: s3Opts.SECRET_ACCESS_KEY,
+    region: s3Opts.REGION,
+    sslEnabled: true,
+    //endpoint: 's3.yourdomain.com',
+    // any other options are passed to new AWS.S3()
+    // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property
+  },
+});
+
+var params = {
+  localDir: path.resolve('dist'),
+  deleteRemoved: true, // default false, whether to remove s3 objects
+  // that have no corresponding local file.
+
+  s3Params: {
+    Bucket: s3Opts.BUCKET,
+    ACL: 'public-read',
+    // other options supported by putObject, except Body and ContentLength.
+    // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
+  },
+};
+
+var uploader = client.uploadDir(params);
+
+uploader.on('error', function(err) {
+  console.error('unable to sync:', err.stack);
+});
+
+uploader.on('progress', function() {
+  const percentage = uploader.progressAmount / uploader.progressTotal;
+  if (percentage % 10 === 0) console.log('progress', percentage);
+});
+
+uploader.on('end', function() {
+  console.log('done uploading');
+});
