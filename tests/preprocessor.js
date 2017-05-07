da74b9420a8b3b0193ccd026a54ffd177bfee842
Travis Matthews <trav.matth@gmail.com>
2017-05-06 21:48:02 -0400

    trying to inject assets into pug

--- tests/preprocessor.js
+++ tests/preprocessor.js
@@ -1,14 +1,23 @@
-//const pug = require('pug');
+const pug = require('pug');
+
+const locals = filename => ({
+  filename,
+  files: {
+    js: [''],
+    css: [''],
+  },
+});
 
 module.exports = {
   process(src, filePath) {
-    console.log('here');
     try {
-      //const template = pug.compile(src);
+      const template = pug.render(src, locals(filePath));
+      console.log('template ', template);
       //const html = template();
       //const content = JSON.stringify(html);
       //return `module.exports = 'success';`;
     } catch (error) {
+      console.log(error);
       //return `module.exports = 'error';`;
     }
     return `module.exports = 'success';`;
