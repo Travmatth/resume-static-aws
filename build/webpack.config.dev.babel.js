22ade9c1983f4e34bdb399bb6ffa94a3c0ae0bd4
Travis Matthews <trav.matth@gmail.com>
2017-06-06 21:49:41 -0400

    removed build from .gitignore

---
+++ build/webpack.config.dev.babel.js
@@ -0,0 +1,52 @@
+/* @flow */
+
+import webpack from 'webpack';
+import common from './common';
+import merge from 'webpack-merge';
+import pageConfigs from './directory';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import FlowStatusWebpackPlugin from 'flow-status-webpack-plugin';
+
+const newConfigs = pageConfigs(page => {
+  return {
+    devtool: 'cheap-eval-source-map',
+
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          exclude: /node_modules/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader', 'postcss-loader', 'sass-loader'],
+          }),
+        },
+        {
+          test: /\.(mp3|wav)$/,
+          exclude: /node_modules/,
+          use: 'file-loader',
+        },
+      ],
+    },
+
+    devServer: {
+      stats: {
+        chunks: false,
+      },
+    },
+
+    plugins: [
+      new FlowStatusWebpackPlugin({
+        onSuccess: stdout => console.log(stdout),
+        onError: stdout => console.log(stdout),
+      }),
+      new ExtractTextPlugin('[name].css'),
+    ],
+
+    output: {
+      filename: '[name].js',
+    },
+  };
+});
+
+export default merge.multiple(common, newConfigs);
---
+++ build/webpack.config.prod.babel.js
@@ -0,0 +1,41 @@
+/* @flow */
+
+import common from './common';
+import pageConfigs from './directory';
+import webpack from 'webpack';
+import merge from 'webpack-merge';
+import autoprefixer from 'autoprefixer';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+const newConfigs = pageConfigs(page => {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          exclude: /node_modules/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader', 'postcss-loader', 'sass-loader'],
+          }),
+        },
+        {
+          test: /\.(mp3|wav)$/,
+          exclude: /node_modules/,
+          use: 'file-loader',
+        },
+      ],
+    },
+
+    plugins: [
+      new ExtractTextPlugin('[name].[hash].css'),
+      new webpack.optimize.UglifyJsPlugin(),
+    ],
+
+    output: {
+      filename: '[name].[hash].js',
+    },
+  };
+});
+
+export default merge.multiple(common, newConfigs);
