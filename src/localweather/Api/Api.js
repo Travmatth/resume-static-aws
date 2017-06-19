1d8db5af6ff33526013f7792943ef06bc33652c5
Travis Matthews <trav.matth@gmail.com>
2017-06-19 13:07:39 -0400

    tracking down travis-ci discrepancy

--- src/localweather/Api/Api.js
+++ src/localweather/Api/Api.js
@@ -88,7 +88,9 @@ const parseTime = (time: number) => {
   const hours = duration.getHours() % 12;
   const minutes = duration.getMinutes();
 
-  return { day: dateString(duration), time: `${hours}:${minutes}0` };
+  const t = { day: dateString(duration), time: `${hours}:${minutes}0` };
+  console.log('parseTime time: ', time, ' result: ', t);
+  return t;
 };
 
 const stripDateIfRedundant = (
