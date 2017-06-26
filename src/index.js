9039bd55b45201f81a002ba7a057a81a470e05f0
Travis Matthews <trav.matth@gmail.com>
2017-06-26 01:11:10 -0400

    impl new nav and homepage w bulma, closes #37

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 /* @flow */
 
-document.addEventListener('DOMContentLoaded', () => console.log('index.js'));
+//https://github.com/jgthms/bulma/issues/192
+const registerNavDropdown = () => {
+  const button = document.getElementById('projects-btn');
+  const dropdown = document.querySelector('.dropdown');
+
+  button.addEventListener('click', () => {
+    dropdown.classList.toggle('is-open');
+  });
+};
+
+document.addEventListener('DOMContentLoaded', registerNavDropdown);
