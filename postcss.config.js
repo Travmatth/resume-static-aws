8dc683aab517e2c31f15f6c8e4eec1aac6aa6bfc
Travis Matthews <trav.matth@gmail.com>
2017-06-27 03:36:11 -0400

    refactored postcss autoprefixer targeting

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@
 // Used by the postcss plugin to set autoprefixing of css
 module.exports = {
   autoprefixer: {
-    browsers: ['last 2 version', 'Firefox 15', 'iOS 8'],
+    browsers: ['> 5% in US'],
   },
   // The plugins section is used by postcss-loader with webpack
   plugins: [require('autoprefixer')],
