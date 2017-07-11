1839e9afb2bd301ae2455f18393b12cc7fc27cb6
Travis Matthews <trav.matth@gmail.com>
2017-07-11 16:07:05 -0400

    remove preprocessors

--- tests/preprocessor.js
+++
@@ -1,8 +0,0 @@
-const pug = require('pug');
-
-module.exports = {
-  process(source, filename) {
-    const fn = pug.compile(source, { filename });
-    return `module.exports = '${fn()}';`;
-  },
-};
