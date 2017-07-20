5f55ea5b88ebee4393a94404841f52ae7dc68a08
Travis Matthews <trav.matth@gmail.com>
2017-07-19 23:13:44 -0400

    moved ontouchstart to js with passive := false flag, closes #51

--- src/common/js/entry.js
+++ src/common/js/entry.js
@@ -1,4 +1,6 @@
 import { registerToggle } from 'common/js/handlers';
 
-if (typeof document !== 'undefined')
+if (typeof document !== 'undefined') {
   document.addEventListener('DOMContentLoaded', () => registerToggle());
+  document.body.addEventListener('touchstart', () => {}, false);
+}
