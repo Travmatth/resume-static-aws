a8259b70596db47cd6273f1c6bdb56735dc6604e
Travis Matthews <trav.matth@gmail.com>
2017-09-30 16:17:18 -0400

    coday issue: Enforce Object Literal Shorthand Syntax

--- src/localweather/Models/constants.js
+++ src/localweather/Models/constants.js
@@ -5,8 +5,8 @@ import OPEN_WEATHER_APPID from 'protected/localweather.key';
 
 const ENDPOINT = 'http://api.openweathermap.org/data/2.5/forecast';
 const OPENWEATHER_API_PARAMS = (lat: number, lon: number) => ({
-  lat: lat,
-  lon: lon,
+  lat,
+  lon,
   units: 'imperial',
   APPID: trim(OPEN_WEATHER_APPID),
 });
